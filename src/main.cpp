6e915e97726eeb09456bace5dd8c2ca193cb29ea
Robert Belanec <belanecrobert22@gmail.com>
2021-04-03 21:53:43 +0200

    added commnet parsing and argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+std::vector<std::string> split_string_on_delimiter(std::string s, std::string delimiter)
+{
+	std::vector<std::string> split_strings;
+	size_t pos = 0;
+
+	while ((pos = s.find(delimiter)) != std::string::npos)
+	{
+		split_strings.push_back(s.substr(0, pos));
+		s.erase(0, pos + delimiter.length());
+	}
+
+	split_strings.push_back(s);
+
+	return split_strings;
+}
+
+std::string remove_comments(std::string s)
+{
+	size_t pos = 0;
+	if ((pos = s.find("#")) == std::string::npos) return s;
+
+	return s.substr(0, pos);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -16,10 +42,24 @@ int main(int argc, char **argv)
 	int status;
 	while (1)
 	{
-		std::cout << "# ";
-		std::cin >> line;
+		std::cout << "#> ";
+		std::getline(std::cin, line);
 
 		if (line == "exit") exit(0);
+
+		line = remove_comments(line);
+
+		std::vector<std::string> strings = split_string_on_delimiter(line, " ");
+		
+		std::vector<char*> cstrings;
+		cstrings.reserve(cstrings.size());
+
+		for (const auto &s : strings)
+		{
+			if (s != "") cstrings.push_back(const_cast<char*>(s.c_str()));
+		}
+
+		char **sargv = &cstrings[0];
 
 		pid_t fpid;
 		switch ((fpid = fork()))
@@ -29,7 +69,7 @@ int main(int argc, char **argv)
 				exit(1);
 			
 			case 0:
-				if (execlp(line.c_str(), line.c_str(), NULL) == -1) std::cerr << "rshell: " << line << ": command not found" << std::endl;
+				if (execvp(sargv[0], sargv) == -1) std::cerr << "rshell: " << line << ": command not found" << std::endl;
 				break;
 
 			default:
