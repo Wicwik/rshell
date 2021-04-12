1e8c5d8a02e321e319e3805aedd975243a2772c1
Robert Belanec <belanecrobert22@gmail.com>
2021-04-12 18:01:08 +0200

    need to debug pipe

--- src/main.cpp
+++ src/main.cpp
@@ -51,6 +51,114 @@ std::pair<std::string, std::string> separate(std::string s, std::string delimite
 	return std::pair<std::string, std::string>{s.substr(0, pos), s.substr(pos+1)};
 }
 
+int spawn_process(int in, int out, std::string cmd)
+{
+	int status;
+
+	bool redout = is_redirect_output(cmd);
+	bool redin = is_redirect_input(cmd);
+
+	if (redout)
+	{
+		std::pair<std::string, std::string> cmd_file_pair = separate(cmd, ">");
+		cmd = cmd_file_pair.first;
+
+		std::string file_str = cmd_file_pair.second;
+		file_str.erase(std::remove_if(file_str.begin(), file_str.end(), [](unsigned char x){return std::isspace(x);}), file_str.end());
+
+		out = open(const_cast<char*>(file_str.c_str()), O_WRONLY|O_CREAT|O_TRUNC, 0666);
+		if (-1 == out) std::cerr << "rshell: error while opening file output" << std::endl;
+	}
+
+	if (redin)
+	{
+		std::pair<std::string, std::string> cmd_file_pair = separate(cmd, "<");
+		cmd = cmd_file_pair.first;
+
+		std::string file_str = cmd_file_pair.second;
+		file_str.erase(std::remove_if(file_str.begin(), file_str.end(), [](unsigned char x){return std::isspace(x);}), file_str.end());
+
+		in = open(const_cast<char*>(file_str.c_str()), O_RDONLY);
+		if (-1 == in) std::cerr << "rshell: error while opening file input" << std::endl;
+	}
+
+	char **sargv = nullptr;
+	std::vector<char*> cstrings;
+	if (cmd != "")
+	{
+		std::vector<std::string> strings = split_string_on_delimiter(cmd, " ");
+		cstrings.reserve(cstrings.size());
+
+		for (auto &s : strings)
+		{
+			if (s != "") cstrings.push_back(const_cast<char*>(s.c_str()));
+		}
+		cstrings.push_back(nullptr);
+		sargv = &cstrings[0];
+	}
+
+	pid_t fpid;
+	switch ((fpid = fork()))
+	{
+		case -1:
+			std::cerr << "Error while forking this process with PID: " << fpid << std::endl;
+			exit(1);
+		
+		case 0:
+			if (in != 0)
+			{
+				if (-1 == dup2(in, fileno(stdin))) std::cerr << "rshell: error while redirecting input" << std::endl;
+
+				fflush(stdin);
+				close(in);
+			}
+
+			if (out != 1)
+			{
+				if (-1 == dup2(out, fileno(stdout))) std::cerr << "rshell: error while redirecting output" << std::endl;
+
+				fflush(stdout);
+				close(out);
+			}
+
+			if (-1 == execvp(sargv[0], sargv)) std::cerr << "rshell: " << cmd << ": command not found" << std::endl;
+			break;
+
+		default:
+			wait(&status);
+
+	}
+
+	return status;
+}
+
+int pipe_cmds(std::vector<std::string> cmds)
+{
+	int in = fileno(stdin);
+	int out = fileno(stdout);
+
+	std::string last_cmd = cmds.back();
+	if (cmds.size() > 1)
+	{
+		int fd[2];
+		
+		last_cmd.pop_back();
+
+		for (const auto &cmd : cmds)
+		{
+			pipe(fd);
+
+			spawn_process(in, fd[1], cmd);
+
+			close(fd[1]);
+
+			in = fd[0];
+		}
+	}
+
+	return spawn_process(in, out, last_cmd);
+}
+
 
 int main(int argc, char **argv)
 {
@@ -58,7 +166,6 @@ int main(int argc, char **argv)
 	pid_t ppid = getppid();
 
 	std::cout << "My PID: " << pid << " Parrent PID: " << ppid << std::endl;
-	int status;
 
 	while (1)
 	{
@@ -66,105 +173,17 @@ int main(int argc, char **argv)
 
 		std::string line;
 		std::getline(std::cin, line);
-		//std::cout << line << std::endl;
 
 		if (line == "exit") exit(0);
 
+		line = remove_comments(line);
 		std::vector<std::string> cmds = split_string_on_delimiter(line, ";");
+
 		for (const auto &cmd : cmds)
 		{
 			std::string parsed_cmd = cmd;
 
-			bool redout = is_redirect_output(parsed_cmd);
-			bool redin = is_redirect_input(parsed_cmd);
-
-			int out;
-			int save_out;
-
-			int in;
-			int save_in;
-
-			if (redout)
-			{
-				std::pair<std::string, std::string> cmd_file_pair = separate(parsed_cmd, ">");
-				parsed_cmd = cmd_file_pair.first;
-				//std::cout << parsed_cmd << std::endl;
-
-				std::string file_str = cmd_file_pair.second;
-				file_str.erase(std::remove_if(file_str.begin(), file_str.end(), [](unsigned char x){return std::isspace(x);}), file_str.end());
-
-				out = open(const_cast<char*>(file_str.c_str()), O_WRONLY|O_CREAT, 0666);
-				if (-1 == out) std::cerr << "rshell: error while opening file output" << std::endl;
-
-				save_out = dup(fileno(stdout));
-
-				if (-1 == dup2(out, fileno(stdout))) std::cerr << "rshell: error while redirecting output" << std::endl;
-			}
-
-			if (redin)
-			{
-				std::pair<std::string, std::string> cmd_file_pair = separate(parsed_cmd, "<");
-				parsed_cmd = cmd_file_pair.first;
-
-				std::string file_str = cmd_file_pair.second;
-				file_str.erase(std::remove_if(file_str.begin(), file_str.end(), [](unsigned char x){return std::isspace(x);}), file_str.end());
-
-				in = open(const_cast<char*>(file_str.c_str()), O_RDONLY);
-				if (-1 == in) std::cerr << "rshell: error while opening file input" << std::endl;
-
-				save_in = dup(fileno(stdin));
-
-				if (-1 == dup2(in, fileno(stdin))) std::cerr << "rshell: error while redirecting input" << std::endl;
-			}
-
-			char **sargv = nullptr;
-			std::vector<char*> cstrings;
-			if (parsed_cmd != "")
-			{
-				parsed_cmd = remove_comments(parsed_cmd);
-
-				std::vector<std::string> strings = split_string_on_delimiter(parsed_cmd, " ");
-				cstrings.reserve(cstrings.size());
-
-				for (auto &s : strings)
-				{
-					if (s != "") cstrings.push_back(const_cast<char*>(s.c_str()));
-					// std::cout << cstrings.back() << std::endl;
-				}
-				cstrings.push_back(nullptr);
-				sargv = &cstrings[0];
-			}
-		
-			pid_t fpid;
-			switch ((fpid = fork()))
-			{
-				case -1:
-					std::cerr << "Error while forking this process with PID: " << pid << std::endl;
-					exit(1);
-				
-				case 0:
-					if (-1 == execvp(sargv[0], sargv)) std::cerr << "rshell: " << parsed_cmd << ": command not found" << std::endl;
-					break;
-
-				default:
-					wait(&status);
-
-					if (redout)
-					{
-						fflush(stdout); 
-						close(out);
-
-						dup2(save_out, fileno(stdout));
-					}
-
-					if (redin)
-					{
-						fflush(stdin);
-						close(in);
-
-						dup2(save_in, fileno(stdin));
-					}
-			}
+			pipe_cmds(split_string_on_delimiter(parsed_cmd, "|"));
 		}
 	}
 
